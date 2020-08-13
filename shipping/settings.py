f8c9948563eebce699b97bf860536ea6d18ca5b1
Haron Kibet Rutoh <kibetharon7@gmail.com>
2020-08-13 12:04:19 +0300

    commented out django.contrib.admin

--- shipping/settings.py
+++ shipping/settings.py
@@ -31,7 +31,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
