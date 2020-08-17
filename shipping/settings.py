75649f11d030847a75f2d48303f4692df3caa677
Haron Kibet Rutoh <kibetharon7@gmail.com>
2020-08-17 10:14:16 +0300

    added admin css to staticfiles_dirs

--- shipping/settings.py
+++ shipping/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    '/usr/local/lib/python3.8.2/site-packages/django/contrib/admin/static',
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
